05acefb617bf233906e87a11b87ff7825bb55ede
nuriabeoldan-dev <nuriabeoldan@gmail.com>
2025-12-23 23:19:49 +0100

    Add section About Me

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+// HERO
 const hero = document.querySelector(".hero");
 const heroContent = document.querySelector(".hero-content");
 
@@ -20,7 +21,7 @@ if (window.innerWidth > 768) {
   });
 }
 
-
+// HAMBURGUER MENU
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
@@ -52,5 +53,89 @@ document.addEventListener('DOMContentLoaded', () => {
 function scrollToSection(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// HEADER
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) { // si el usuario ha bajado 50px
+    header.style.opacity = '0.75'; // reduce a 75%
+  } else {
+    header.style.opacity = '1'; // vuelve a opacidad completa
+  }
+});
+
+
+//TEXTO SECCIONES
+
+const aboutSection = document.querySelector(".about-section");
+const aboutText = document.querySelector(".about-text");
+const aboutImage = document.querySelector(".about-image");
+
+if (aboutSection) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+          // Texto entra primero (sutil)
+          aboutText.style.opacity = "1";
+          aboutText.style.transform = "translateY(0)";
+
+          // Imagen entra después (protagonista)
+          setTimeout(() => {
+            aboutImage.style.opacity = "1";
+            aboutImage.style.transform = "translateX(0)";
+          }, 150);
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  observer.observe(aboutSection);
+}
+
+// HABILIDADES SECCION
+
+const skillsSection = document.querySelector(".skills-section");
+const skillFills = document.querySelectorAll(".skill-fill");
+
+if (skillsSection) {
+  const skillsObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          skillFills.forEach(bar => {
+            const level = bar.getAttribute("data-level");
+            bar.style.width = level + "%";
+          });
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  skillsObserver.observe(skillsSection);
+}
+
+// SOFTWARES
+const softwareChart = document.querySelector(".software-chart");
+
+if (softwareChart) {
+  const softwareObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          softwareChart.style.opacity = "1";
+          softwareChart.style.transform = "translateY(0)";
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  softwareObserver.observe(softwareChart);
 }
 
