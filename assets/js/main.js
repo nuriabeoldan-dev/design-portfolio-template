22e88a8785494ad0b54535d0186ea2a37d357d58
nuriabeoldan-dev <nuriabeoldan@gmail.com>
2025-12-24 16:32:51 +0100

    404 error

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,12 @@ if (softwareChart) {
   softwareObserver.observe(softwareChart);
 }
 
+// ERROR 404
+
+document.addEventListener('mousemove', e => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 10;
+  const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+  document.querySelector('.error-content').style.transform =
+    `translate(${x}px, ${y}px)`;
+});
